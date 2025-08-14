3b63a011428b3535d5ae086f8cccca488af3fd83
kZime <kzime@outlook.com>
2025-08-14 18:54:01 -0500

    update css... fix somebug,, add more bugs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
